3a75d760ac390e8b8ebeed45127406f7f4d93346
Kausik Paudel <97453660+kausik10@users.noreply.github.com>
2022-06-28 13:08:08 +0545

    Add files via upload

---
+++ hello_rust/hello_world.rs
@@ -0,0 +1,9 @@
+fn main() //main function without parameter, returns nothing
+{
+	println!("Hello World, from RUST"); 
+}
+/**
+	Rust styling: 4 spaces and not a tab. Noice
+	println! calls rust macro. if it is a functiion ,it would be written without ! 
+	ending with semicolon indicates end of expression and the next one is ready to begin. Most lines of RUST ends with semicolon.
+**/
